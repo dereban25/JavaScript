c45c068cabe27cd54fc7f0872aaa90de94bc3a0d
curl http://installrails.com/update_rubygems.rb | ruby <dereban25@gmail.com>
2017-07-28 15:40:58 +0300

    new lessons update

--- JS_16/16.js
+++ JS_16/16.js
@@ -1,6 +1,13 @@
+/**
+ * Created by dereban on 26.07.2017.
+ */
 document.getElementById('r1').oninput = cssGenerator;
-function cssGenerator(){
-    var div = document.getElementById('test');
-    console.log(this.value);
+function cssGenerator() {
+    var div =document.getElementById('test');
+    // console.log(this.value);
     div.style.borderRadius = this.value + 'px';
+    var out = document.getElementById('out');
+    out.innerHTML = '-webkit-border-radius: '+ this.value + 'px;\n';
+    out.innerHTML += 'border-radius: '+this.value+'px;';
+
 }
